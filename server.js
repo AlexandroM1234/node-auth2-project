93c55a5c9afa6e24813974ec7dcf5baa105861f9
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-21 15:47:56 -0400

    getting users work

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
-const userRouter = require("./userRouter");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
 server.use(express.json());
+server.use("/api/users", userRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "is up and running" });
