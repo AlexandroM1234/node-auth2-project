1529ee1c8dcd6750ff205db06d2a7a07fce75dc4
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-21 16:35:23 -0400

    it works

--- users/user-model.js
+++ users/user-model.js
@@ -2,8 +2,18 @@ const db = require("../database/db-config");
 
 module.exports = {
   find,
+  addUser,
+  findBy,
 };
 
 function find() {
-  return db("users").select("id", "username");
+  return db("users").select("id", "username", "department");
+}
+
+function addUser(newUser) {
+  return db("users").insert(newUser, "id");
+}
+
+function findBy(something) {
+  return db("users").where(something);
 }
