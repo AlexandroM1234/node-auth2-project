538edda2cea8389a215ee7b8636abd9cbdc99635
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-21 15:29:24 -0400

    db made

--- database/migrations/20200421151606_making_user_table.js
+++ database/migrations/20200421151606_making_user_table.js
@@ -1,8 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (tbl) => {
+    tbl.increments();
 
-exports.up = function(knex) {
-  
+    tbl.string("username").notNullable().unique();
+    tbl.string("password").notNullable();
+    tbl.string("department").notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users");
 };
